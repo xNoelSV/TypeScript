83d7bf5eacfb2567278f33749b2026b4febe81a5
Noel Sariñena Varela <noelsava25@gmail.com>
2024-03-19 19:54:33 +0100

    Hola mundo en TypeScript + Watch

---
+++ Bases/app.js
@@ -0,0 +1,9 @@
+"use strict";
+const msg = 'Hola mundo';
+console.log(msg);
+const hero = {
+    name: 'ironman',
+    age: 45
+};
+hero.age = 50;
+console.log(hero.age + 1);
---
+++ Bases/app.ts
@@ -0,0 +1,11 @@
+
+
+const msg: string = 'Hola mundo'
+console.log(msg);
+
+const hero = {
+  name: 'ironman',
+  age: 45
+}
+hero.age = 50;
+console.log(hero.age + 1)
