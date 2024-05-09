1dcd132b8ac152b8611f6f831c77f4ce1d171b05
TCM-Noel <noelsava25@gmail.com>
2024-05-09 19:20:53 +0200

    ES6

---
+++ Bases/ES6/desestructuracion.ts
@@ -0,0 +1,27 @@
+(() => {
+
+  type Avengers = {
+    nick: string;
+    ironman: string;
+    vision: string;
+    activo: boolean;
+    poder: number;
+  }
+
+  const avengers:Avengers = {
+    nick: 'Samuel L. Jackson',
+    ironman: 'Robert Downey Jr.',
+    vision: 'Paul Bettany',
+    activo: true,
+    poder: 1500
+  }
+
+  //const { poder, vision } = avengers;
+  //console.log( poder.toFixed(2), vision.toUpperCase() )
+  const printAvenger = ( { ironman, ...resto }:Avengers ) => {
+    console.log(ironman, resto);
+  }
+
+  console.log(printAvenger);
+
+})
---
+++ Bases/ES6/let-var.ts
@@ -0,0 +1,15 @@
+(() => {
+
+  //console.log(a);
+
+  const nombre:string = 'Noel';
+
+  const getName = ():void => {
+    console.log('viejo getName');
+  }
+
+  //getName = () => { console.log('Nuevo getName') }
+
+  getName();
+
+})
