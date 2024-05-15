e1faf9261ba545934283322e3b86aa0d6dbddad9
TCM-Noel <noelsava25@gmail.com>
2024-05-15 18:32:25 +0200

    Decoradores

--- ts-node/src/decorators/pokemon-class.ts
+++ ts-node/src/decorators/pokemon-class.ts
@@ -14,7 +14,6 @@ const blockPrototype = function( constructor:Function ) {
 
 function CheckValidPokemonId() {
   return function( target:any, propertyKey:string, descriptor:PropertyDescriptor ) {
-
     const originalMethod = descriptor.value;
     descriptor.value = ( id:number ) => {
       if (id < 1 || id > 800) {
@@ -25,12 +24,34 @@ function CheckValidPokemonId() {
   }
 }
 
+function readOnly( isWritable: boolean = true ):Function {
+  return function(target:any, propertyKey: string) {
+    const descriptor: PropertyDescriptor = {
+      get() {
+        console.log(this)
+        return 'Noel'
+      },
+      set(this, val) {
+        // console.log(this, val)
+        Object.defineProperty( this, propertyKey, {
+          value: val,
+          writable: !isWritable,
+          enumerable: false
+        })
+      }
+    }
+
+    return descriptor;
+  }
+}
+
 
 
 @blockPrototype
 @printToConsoleConditional( true )
 export class Pokemon {
 
+  @readOnly(true)
   public publicApi:string = 'https://pokeapi.co';
 
   constructor(
--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -3,4 +3,5 @@ import { Pokemon } from "./decorators/pokemon-class";
 const charmander = new Pokemon('Charmander');
 /* (Pokemon.prototype as any).customName = 'Pikachu'; */
 
-console.log(charmander.savePokemonToDB(50))
+charmander.publicApi = 'https://portafoliodenoel.netlify.com';
+console.log(charmander)
